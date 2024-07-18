5fb954f0ced27a849eaff5c88cba61915398731a
Aritra Ghorai <aritra@gmail.com>
2024-07-18 16:27:24 +0530

    Added new config

---
+++ .config/nvim/lua/plugins/java.lua
@@ -0,0 +1,10 @@
+return {
+  -- https://github.com/JavaHello/spring-boot.nvim
+  "JavaHello/spring-boot.nvim",
+  ft = "java",
+  dependencies = {
+    "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
+    "ibhagwan/fzf-lua", -- 可选
+  },
+  opts = {},
+}
---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,14 @@
+return {
+  "epwalsh/obsidian.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/File/work/Common/Obsadian/",
+      },
+    },
+  },
+}
